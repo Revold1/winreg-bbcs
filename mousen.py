c68dcabf69158144af660e10e030b8a40dcf97bc
Revold1 <34865092+Revold1@users.noreply.github.com>
2020-05-14 21:31:03 +0800

    Update mousen.py

--- mousen.py
+++ mousen.py
@@ -4,9 +4,7 @@ REG_PATH = r"Control Panel\Mouse"
 
 def set_reg(name, value):
     try:
-        winreg.CreateKey(winreg.HKEY_CURRENT_USER, REG_PATH)
-        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, 
-                                       winreg.KEY_WRITE)
+        registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, REG_PATH, 0, winreg.KEY_WRITE)
         winreg.SetValueEx(registry_key, name, 0, winreg.REG_SZ, value)
         winreg.CloseKey(registry_key)
         return True
